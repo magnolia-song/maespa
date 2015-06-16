2edaf972173c0f1e7055efe81acceeb8a7554089
Remko Duursma <remkoduursma@gmail.com>
2015-06-16 13:53:29 +0200

    Re #10, added license to two remaining files.

--- unstor.f90
+++ unstor.f90
@@ -1,5 +1,25 @@
 !**********************************************************************
 ! UNSTOR.FOR
+
+!=======================================================================================
+! Copyright 2015 Remko Duursma, Belinda Medlyn, Mathias Christina, Guerric le Maire
+!---------------------------------------------------------------------------------------
+! this file is part of MAESPA.
+!
+! MAESPA is free software: you can redistribute it and/or modify
+! it under the terms of the gnu general public license as published by
+! the free software foundation, either version 2 of the license, or
+! (at your option) any later version.
+!
+! MAESPA is distributed in the hope that it will be useful,
+! but without any warranty; without even the implied warranty of
+! merchantability or fitness for a particular purpose.  see the
+! gnu general public license for more details.
+!
+! you should have received a copy of the gnu general public license
+! along with MAESPA.  if not, see <http://www.gnu.org/licenses/>.
+!=======================================================================================
+
 ! RAD FEB. 2009, BASED ON MAESUS.FOR
 !
 ! This file contains all the subroutines related to understorey radiation
